ec41e26d9aae5a0b3c5e169c383cfbb30fbb1384
jcnpereira <casimiro@ipt.pt>
2026-04-27 12:43:10 +0100

    change the interaction with DegreeFK

--- Aulas/Aulas/Pages/Students/Create.cshtml.cs
+++ Aulas/Aulas/Pages/Students/Create.cshtml.cs
@@ -2,44 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+
 using Aulas.Data;
 using Aulas.Data.Model;
 
-namespace Aulas.Pages.Students
-{
-    public class CreateModel : PageModel
-    {
-        private readonly Aulas.Data.ApplicationDbContext _context;
+namespace Aulas.Pages.Students {
 
-        public CreateModel(Aulas.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+   public class CreateModel:PageModel {
 
-        public IActionResult OnGet()
-        {
-        ViewData["DegreeFK"] = new SelectList(_context.Degrees, "Id", "Name");
+      private readonly ApplicationDbContext _context;
+
+      public CreateModel(ApplicationDbContext context) {
+         _context = context;
+      }
+
+
+
+      public IActionResult OnGet() {
+
+         // list of options to appear in the dropdown list for the Degree property
+         ViewData["DegreeFK"] = new SelectList(_context.Degrees.OrderBy(d => d.Name), "Id", "Name");
+     
+         return Page();
+      }
+
+
+      [BindProperty]
+      public Student Student { get; set; } = default!;
+
+
+
+      // For more information, see https://aka.ms/RazorPagesCRUD.
+      public async Task<IActionResult> OnPostAsync() {
+       
+         if(!ModelState.IsValid) {
+            // list of options to appear in the dropdown list for the Degree property
+            ViewData["DegreeFK"] = new SelectList(_context.Degrees.OrderBy(d => d.Name), "Id", "Name");
+
             return Page();
-        }
+         }
 
-        [BindProperty]
-        public Student Student { get; set; } = default!;
+         _context.Students.Add(Student);
+         await _context.SaveChangesAsync();
 
-        // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Students.Add(Student);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
-    }
+         return RedirectToPage("./Index");
+      }
+   }
 }
