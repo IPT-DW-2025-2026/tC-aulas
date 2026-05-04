1326c79703f17b8451ec651b4172e38d982c7029
jcnpereira <casimiro@ipt.pt>
2026-05-04 09:52:09 +0100

    change of TuitionFee attribute

---
+++ Aulas/Aulas/Data/Migrations/20260504084845_ChangeTuitionFeeDecimalPrecision.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aulas.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTuitionFeeDecimalPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TuitionFee",
+                table: "AppUsers",
+                type: "decimal(9,2)",
+                precision: 9,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TuitionFee",
+                table: "AppUsers",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9,2)",
+                oldPrecision: 9,
+                oldScale: 2,
+                oldNullable: true);
+        }
+    }
+}
