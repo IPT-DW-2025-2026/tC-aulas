08fd7cde6441ee2cf187c40387d3b0a533c010f9
jcnpereira <casimiro@ipt.pt>
2026-05-04 10:16:28 +0100

    convert data from TuitionFeeAux to TuitionFee

--- Aulas/Aulas/Pages/Students/Create.cshtml.cs
+++ Aulas/Aulas/Pages/Students/Create.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 using Aulas.Data;
 using Aulas.Data.Model;
+using System.Globalization;
 
 namespace Aulas.Pages.Students {
 
@@ -26,7 +27,7 @@ namespace Aulas.Pages.Students {
 
          // list of options to appear in the dropdown list for the Degree property
          ViewData["DegreeFK"] = new SelectList(_context.Degrees.OrderBy(d => d.Name), "Id", "Name");
-     
+
          return Page();
       }
 
@@ -38,7 +39,7 @@ namespace Aulas.Pages.Students {
 
       // For more information, see https://aka.ms/RazorPagesCRUD.
       public async Task<IActionResult> OnPostAsync() {
-       
+
          if(!ModelState.IsValid) {
             // list of options to appear in the dropdown list for the Degree property
             ViewData["DegreeFK"] = new SelectList(_context.Degrees.OrderBy(d => d.Name), "Id", "Name");
@@ -46,8 +47,24 @@ namespace Aulas.Pages.Students {
             return Page();
          }
 
-         _context.Students.Add(Student);
-         await _context.SaveChangesAsync();
+         // Convert the TuitionFeeAux string to a decimal
+         // and assign it to the TuitionFee property
+         Student.TuitionFee = Convert.ToDecimal(Student.TuitionFeeAux.Replace('.', ','),
+                                                new CultureInfo("pt-PT"));
+
+         try {
+            _context.Students.Add(Student);
+            await _context.SaveChangesAsync();
+         }
+         catch(Exception) {
+            // do not forget that YOU MUST handle the exception
+            // in a way that is appropriate for your application
+            // DO NOT USE a 'throw' in a production application,
+            // because it will present to many details about the exception to the user,
+            // which IS a security risk.
+            throw;
+         }
+
 
          return RedirectToPage("./Index");
       }
